cd77f80fc0f61439adc233422f8fedfbba83caf3
mashharuki <avp.104.106.107@ae.auone-net.jp>
2023-06-10 17:32:52 +0900

    フロントエンドのソースを追加する。

---
+++ frontend/README.md
@@ -0,0 +1,12 @@
+# XRP Ledger NFT Starter
+
+![](/public/learn-banner.png)
+
+このリポジトリはUNCHAINのXRPL NFT Makerのスターターキットです。
+
+## はじめてみよう
+
+1. リポジトリをクローンする
+2. `npm install` を実行する
+3. `npm run start` を実行する
+4. コードを書きましょう！
---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { NftMinter } from './components/NftMinter';
+
+/**
+ * App Component
+ * @returns 
+ */
+function App() {
+  return (
+    <div className="App">
+      <NftMinter/>
+    </div>
+  );
+}
+
+export default App;
