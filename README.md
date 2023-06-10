dc00f2b6ce3b72ae0aa5790be69b2e20b082b406
mashharuki <avp.104.106.107@ae.auone-net.jp>
2023-06-10 17:48:17 +0900

    セクション3まで追加

--- README.md
+++ README.md
@@ -1,20 +1,21 @@
 # XRP-NFT-Maker
-XRPと連動するNFT Makerアプリです。
+
+XRP と連動する NFT Maker アプリです。
 
 ### Generated Testnet Faucet Info for. Dev
 
-- Your Testnet Credentials. 
-Address
-rhp1Z2kmV4ES9fCEkCG7nDSADu6WT6xJQE
-- Secret. 
-sEdTJe9xiEFKxY17A644fxLCPHBVuiX
-- Balance. 
-10,000 XRP
-- Sequence Number. 
-38535107
-
+- Your Testnet Credentials.
+  Address
+  rhp1Z2kmV4ES9fCEkCG7nDSADu6WT6xJQE
+- Secret.
+  sEdTJe9xiEFKxY17A644fxLCPHBVuiX
+- Balance.
+  10,000 XRP
+- Sequence Number.
+  38535107
 
 ### 参考文献
+
 1. [Xumm](https://xumm.app/)
 2. [XRP Faucet](https://xrpl.org/ja/xrp-testnet-faucet.html)
 3. [XRP Docs](https://xrpl.org/ja/docs.html)
@@ -22,3 +23,5 @@ sEdTJe9xiEFKxY17A644fxLCPHBVuiX
 5. [【NPM】 Numm SDK](https://www.npmjs.com/package/xumm)
 6. [XRP NFT-Explorer](https://test.bithomp.com/nft-explorer)
 7. [【NPM】 xrpl](https://www.npmjs.com/package/xrpl)
+8. [XLP-24](https://github.com/XRPLF/XRPL-Standards/discussions/69)
+9. [NFT.Storage](https://nft.storage/)
--- frontend/src/components/NftMinter/index.jsx
+++ frontend/src/components/NftMinter/index.jsx
@@ -1,15 +1,120 @@
 import { Button } from "@mui/material";
+import { extractAffectedNFT } from "@xrplkit/txmeta";
+import { Buffer } from "buffer";
+import { NFTStorage } from "nft.storage";
+import { useEffect, useState } from "react";
+import { XrplClient } from 'xrpl-client';
 import { Xumm } from "xumm";
 import "./index.css";
 
-// const xumm = new Xumm('api-key')
+// Xummインスタンスを生成する。
+const xumm = new Xumm('api-key');
+const nftStorage = new NFTStorage({ 
+  token: "api-key",
+});
 
+/**
+ * NftMinter Component
+ * @returns 
+ */
 export const NftMinter = () => {
+  const [account, setAccount] = useState(undefined);
+  const [file, setFile] = useState(undefined);
+
+  useEffect(() => {
+    xumm.on("success", async () => {
+      setAccount(await xumm.user.account);
+    });
+  }, []);
+
+  /**
+   * connect method
+   */
+  const connect = () => {
+    xumm.authorize();
+  };
+
+  /**
+   * uploadImage method
+   * @param {*} e 
+   */
+  const uploadImage = (e) => {
+    const files = e.target.files;
+    setFile(files[0])
+  };
+
+  /**
+   * mint method
+   * @returns 
+   */
+  const mint = async () => {
+    if (!file) {
+      alert("画像ファイルを選択してください！");
+      return;
+    }
+
+    // 画像とメタデータをIPFSにアップロード
+    const { url } = await nftStorage.store({
+      schema: "ipfs://QmNpi8rcXEkohca8iXu7zysKKSJYqCvBJn3xJwga8jXqWU",
+      nftType: "art.v0",
+      image: file,
+      name: "some name",
+      description: "some description",
+    });
+    // Xummにトランザクションデータを送信
+    const payload = await xumm.payload.createAndSubscribe({
+      TransactionType: "NFTokenMint",
+      NFTokenTaxon: 0,
+      Flags: 8,
+      URI: Buffer.from(url).toString("hex"),
+    });
+
+    payload.websocket.onmessage = (msg) => {
+      const data = JSON.parse(msg.data.toString());
+      // トランザクションへの署名が完了/拒否されたらresolve
+      if (typeof data.signed === "boolean") {
+        payload.resolve({ signed: data.signed, txid: data.txid });
+      }
+    };
+
+    // resolveされるまで待機
+    const { signed, txid } = await payload.resolved;
+
+    if (!signed) {
+      alert("トランザクションへの署名は拒否されました！");
+      return;
+    }
+    // テストネットからトランザクションの情報を取得
+    const client = new XrplClient("wss://testnet.xrpl-labs.com");
+    const txResponse = await client.send({
+      command: "tx",
+      transaction: txid,
+    });
+    // トランザクション情報からNFTの情報を取得
+    const nftoken = extractAffectedNFT(txResponse);
+    alert('NFTトークンが発行されました！')
+    window.open(`https://test.bithomp.com/nft/${nftoken.NFTokenID}`, "_blank");
+  };
+
   return (
     <div className="nft-minter-box">
       <div className="title">XRP NFT</div>
+      <div className="account-box">
+        <div className="account">
+          {account}
+        </div>
+        <Button 
+          variant="contained" 
+          onClick={connect}
+        >
+          connect
+        </Button>
+      </div>
       <div className="image-box">
-        <Button variant="contained">
+        <Button 
+          variant="contained"
+          onChange={uploadImage}  
+        >
           ファイルを選択
           <input
             className="imageInput"
@@ -17,6 +122,23 @@ export const NftMinter = () => {
             accept=".jpg , .jpeg , .png"
           />
         </Button>
+        {file && (
+          <img 
+            src={window.URL.createObjectURL(file)} 
+            alt="nft" 
+            className="nft-image" 
+          />
+        )}
+        {account && (
+          <div>
+            <Button 
+              variant="outlined" 
+              onClick={mint}
+            >
+              mint
+            </Button>
+          </div>
+        )}
       </div>
     </div>
   );
